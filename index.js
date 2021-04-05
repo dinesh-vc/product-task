b3e23875e07f13d950b65050bf128a4674821c20
DineshViitor <dinesh.chavda@viitorcloud.co>
2021-04-05 19:14:35 +0530

    DotEnv File Added

--- index.js
+++
@@ -1,5 +0,0 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const user = require('./models/user');
-const product = require('./models/product');
-const order = require('./models/order');
